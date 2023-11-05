25c14bc78dcc71b4002ffba02e482b1cb44c5851
Lucía Madsen <madsen.lu@gmail.com>
2023-11-05 10:58:19 -0300

    toque

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import CartWidget from './CartWidget'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, MenuItem, Button } from '@mui/material';
+import CartWidget from './CartWidget';
+
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav className="navbar">
-        <Link to="/" className="logo"><h1>BAYSIDE TEAM</h1></Link>
-        <ul className="menu">
-            <li><Link className="menu-link" to="/productos">Productos</Link></li>
-            <li><Link className="menu-link" to="/productos/indumentaria">Indumentaria</Link></li>
-            <li><Link className="menu-link" to="/productos/accesorios">Accesorios</Link></li>
-            <li><Link className="menu-link" to="/productos/equipos">Equipos</Link></li>
-            <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
-            <li><Link className="menu-link" to="/contacto">Contacto</Link></li>
-            <li><CartWidget /></li>
-        </ul>
+      <Link to="/" className="logo">
+        <h1>BAYSIDE TEAM</h1>
+        
+      </Link>
+      <ul className="menu">
+
+        <li>
+          <Link className="menu-link" to="/nosotros">
+            Nosotros
+          </Link>
+        </li>
+
+        <li>
+          <Link className="menu-link" to="/productos/equipos">
+            Equipos
+          </Link>
+        </li>
+        <li>
+          <Link className="menu-link" to="/contacto">
+            Contacto
+          </Link>
+        </li>
+
+        <li>
+          <Button
+            className="menu-link"
+            onClick={handleClick}
+          >
+            Productos
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem>
+              <Link to="/productos/indumentaria" className="submenu-link">
+                Indumentaria
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/productos/accesorios" className="submenu-link">
+                Accesorios
+              </Link>
+            </MenuItem>
+          </Menu>
+        </li>
+        <li>
+          <CartWidget />
+        </li>
+      </ul>
     </nav>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
+
+
